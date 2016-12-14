8e93eff0efd7ff11ef0e834ab8fca98befb36fd9
drgomesp <daniel.ribeiro@propertyfinder.ae>
2016-12-14 17:32:23 +0400

    Added more scenarios to struct test

--- tests/generator/struct_test.go
+++ tests/generator/struct_test.go
@@ -79,3 +79,49 @@ func (r *Foo) Bar(first bool, second int, third string) (first bool, second int)
 		})
 	})
 }
+
+func TestGeneratingStructWithFieldsAndMethods(t *testing.T) {
+	Convey("Given a struct generator instance for struct with fields and methods", t, func() {
+		gen := generator.StructGenerator{
+			Name: "Foo",
+			Fields: []generator.StructFieldGenerator{
+				generator.StructFieldGenerator{
+					Name: "FieldA",
+					Kind: reflect.Bool,
+				},
+				generator.StructFieldGenerator{
+					Name: "FieldB",
+					Kind: reflect.Int,
+				},
+				generator.StructFieldGenerator{
+					Name: "FieldC",
+					Kind: reflect.String,
+				},
+			},
+			Methods: []generator.StructMethodGenerator{
+				generator.StructMethodGenerator{
+					Name: "Bar",
+					Parameters: []meta.Parameter{
+						meta.Parameter{Name: "first", Type: reflect.Bool},
+						meta.Parameter{Name: "second", Type: reflect.Int},
+						meta.Parameter{Name: "third", Type: reflect.String},
+					},
+					Returns: []meta.Parameter{
+						meta.Parameter{Name: "first", Type: reflect.Bool},
+						meta.Parameter{Name: "second", Type: reflect.Int},
+					},
+				},
+			},
+		}
+
+		Convey("Then an empty struct should be generated correctly", func() {
+			So(gen.Generate(), ShouldEqual, `type Foo struct {
+	FieldA bool
+	FieldB int
+	FieldC string
+}
+
+func (r *Foo) Bar(first bool, second int, third string) (first bool, second int) {}`)
+		})
+	})
+}
